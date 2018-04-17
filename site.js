d193cbc9d6a3fb9061f232ca276e875b9ae7adac
Sean Kelliher <sbkelliher@icloud.com>
2018-04-17 17:11:18 -0400

    Add graphic, revise format for JSLint

--- site.js
+++ site.js
@@ -1,49 +1,67 @@
+//To avoid confusion, remember that Universal Coordinated Time (UCT) equals Greenwich Mean Time (GMT), there is no difference.
+let days;
 
-window.addEventListener("load", function () {
-"use strict";
+//When the DOM Content is Loaded, invoke the "get days" function
+document.addEventListener("DOMContentLoaded", function () {
+    "use strict";
 
-    //The number of milliseconds between January 1, 1970 at midnight UTC and November 3, 2020 at midnight local time.
-    var electionDay = Date.parse("2020-11-03T00:00:00");
-
-    //The current date and time (now)
-    var todayDt = new Date();
-
-    //The number of milliseconds between January 1, 1970 at midnight UTC and now (local time).
-    var todayMs = todayDt.getTime();
-
-    //Subtraction: election day milliseconds minus today milliseconds
-    var totalMs = (electionDay - todayMs);
-
-    //The number of milliseconds in a day: 86400000
-    var totalDays = (totalMs / 86400000);
-
-    //Math.floor rounds result down
-    var totalDaysMf = Math.floor(totalDays);
-
-    //Change Integer to string
-    var totalDaysMfSt = totalDaysMf.toString();
-
-    //Check length of string
-    var totalDaysMfStLength = totalDaysMfSt.length;
-
-    //If statements to determine if number needs a comma
-    if (totalDaysMfStLength <= 3) {
-        //Retrieve span id on page
-        var daysToVoting = document.getElementById("days-to-voting");
-
-        //Create text node for id
-        var daysToVotingText = document.createTextNode(totalDaysMfSt);
-
-        //Append text node to id
-        daysToVoting.appendChild(daysToVotingText);
-        } else if (totalDaysMfStLength > 3) {
-            //Retrieve span id on page
-            var daysToVotingLg = document.getElementById("days-to-voting");
-
-            //Create text node for id
-            var daysToVotingTextLg = document.createTextNode(totalDaysMfSt.substr(0, 1) + "," + totalDaysMfSt.substr(1, 4));
-
-            //Append text node to id
-            daysToVotingLg.appendChild(daysToVotingTextLg);   
-            }
+    //Invoke the "get days" function
+    days.getDays();
 });
+
+days = {
+    getDays: function () {
+        "use strict";
+
+        //create the date objects
+        const election = new Date("November 3, 2020 00:00:00 GMT+00:00");
+        const today = new Date();
+
+        //return number of milliseconds between the objects' dates and Jan. 1, 1970 at midnight. All UCT.
+        const electionMs = election.getTime();
+        const todayMs = today.getTime();
+
+        //return the time zone offset (difference between UCT and local time) in minutes
+        //Easy to get confused here - if your local time is behind UCT, method returns a positive number. If your local time is ahead of UCT, method returns a negative number
+        const electionOffset = election.getTimezoneOffset();
+        const todayOffset = today.getTimezoneOffset();
+
+        //convert minutes to seconds, then milliseconds
+        const electionOffsetMs = (electionOffset * 60) * 1000;
+        const todayOffsetMs = (todayOffset * 60) * 1000;
+
+        //ternary statements - careful here, remember the note above about time zone difference and negative/positive numbers
+        const electionLocal = (electionOffsetMs > 0)
+            ? electionMs - electionOffsetMs
+            : electionMs + electionOffsetMs;
+        const todayLocal = (electionOffsetMs > 0)
+            ? todayMs - todayOffsetMs
+            : todayMs + todayOffsetMs;
+        const daysInMs = electionLocal - todayLocal;
+
+        //The number of milliseconds in a day: 86400000
+        const totalDays = (daysInMs / 86400000);
+
+        //Math.floor rounds result down
+        const totalDaysMf = Math.floor(totalDays);
+
+        //Change integer to string
+        const totalDaysMfSt = totalDaysMf.toString();
+
+        //Check length of string
+        const totalDaysMfStLength = totalDaysMfSt.length;
+
+        //Get element, create text node for it, append (some substra work to add comma if number is over three digits)
+        const daysToVoting = document.getElementById("days-to-voting");
+        const daysToVotingText = (totalDaysMfStLength <= 3)
+            ? document.createTextNode(totalDaysMfSt)
+            : document.createTextNode(totalDaysMfSt.substr(0, 1) + "," + totalDaysMfSt.substr(1, 4));
+        daysToVoting.appendChild(daysToVotingText);
+
+        //position the needle on the gauge
+        //There are four years or 1460 days between election days, that's 14.6 days per percentage point.
+        const needlePosition = (1460 - totalDaysMf) / 14.6;
+        document.getElementById("needle").style.left = needlePosition + "%";
+
+    } //close function
+}; //close variable
