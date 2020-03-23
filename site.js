bb30c487846e5bd092eadfb26a80020a776ca7e5
Sean Kelliher <sbkelliher@icloud.com>
2020-03-22 23:12:36 -0400

    Add nested if statements that check world's 23 time zones.

--- site.js
+++ site.js
@@ -90,6 +90,227 @@ days = {
         //Create mutable variable for "adjusted" day.
         let until2020DayAdjusted;
 
+        //If/else statements check world's 23 time zones.
+        //If UTC is "x" minutes ahead or behind local time...
+        //and UTC time is within this time of day...
+        //add or subtract a day from the countdown timer.
+        //Ensures timer is accurate and changes at midnight local time.
+        if (Math.abs(offSet) === 60) { //UTC: 1 hour plus/minus.
+            if (hour === 0 && minute <= 59 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                } else if (offSet < 0) {
+                    until2020DayAdjusted = until2020Day - 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 120) { //UTC: 2 hrs plus/minus.
+            if (hour >= 0 && hour <= 1 && minute <= 59 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                } else if (offSet < 0) {
+                    until2020DayAdjusted = until2020Day - 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 180) { //UTC: 3 hrs plus/minus.
+            if (hour >= 0 && hour <= 2 && minute <= 59 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                } else if (offSet < 0) {
+                    until2020DayAdjusted = until2020Day - 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 210) { //UTC: 3.5 hrs plus/minus.
+            if (hour >= 0 && hour <= 3 && minute <= 29 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                } else if (offSet < 0) {
+                    until2020DayAdjusted = until2020Day - 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 240) { //UTC: 4 hrs plus/minus.
+            if (hour >= 0 && hour <= 3 && minute <= 59 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                } else if (offSet < 0) {
+                    until2020DayAdjusted = until2020Day - 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 270) { //UTC: 4.5 hrs plus. No minus.
+            if (hour >= 0 && hour <= 4 && minute <= 29 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 300) { //UTC: 5 hrs plus/minus.
+            if (hour >= 0 && hour <= 4 && minute <= 59 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                } else if (offSet < 0) {
+                    until2020DayAdjusted = until2020Day - 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 330) { //UTC: 5.5 hrs plus. No minus.
+            if (hour >= 0 && hour <= 5 && minute <= 29 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 345) { //UTC: 5.75 hrs plus. No minus.
+            if (hour >= 0 && hour <= 5 && minute <= 44 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 360) { //UTC: 6 hrs plus/minus.
+            if (hour >= 0 && hour <= 5 && minute <= 59 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                } else if (offSet < 0) {
+                    until2020DayAdjusted = until2020Day - 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 390) { //UTC: 6.5 hrs plus. No minus.
+            if (hour >= 0 && hour <= 6 && minute <= 29 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 420) { //UTC: 7 hrs plus/minus.
+            if (hour >= 0 && hour <= 6 && minute <= 59 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                } else if (offSet < 0) {
+                    until2020DayAdjusted = until2020Day - 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 480) { //UTC: 8 hrs plus/minus.
+            if (hour >= 0 && hour <= 7 && minute <= 59 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                } else if (offSet < 0) {
+                    until2020DayAdjusted = until2020Day - 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 525) { //UTC: 8.75 hrs plus. No minus.
+            if (hour >= 0 && hour <= 8 && minute <= 44 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 540) { //UTC: 9 hrs plus/minus.
+            if (hour >= 0 && hour <= 8 && minute <= 59 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                } else if (offSet < 0) {
+                    until2020DayAdjusted = until2020Day - 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 570) { //UTC: 9.5 hrs plus/minus.
+            if (hour >= 0 && hour <= 9 && minute <= 29 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                } else if (offSet < 0) {
+                    until2020DayAdjusted = until2020Day - 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 600) { //UTC: 10 hrs plus/minus.
+            if (hour >= 0 && hour <= 9 && minute <= 59 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                } else if (offSet < 0) {
+                    until2020DayAdjusted = until2020Day - 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 630) { //UTC: 10.5 hrs plus. No minus.
+            if (hour >= 0 && hour <= 10 && minute <= 29 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 660) { //UTC: 11 hrs plus/minus.
+            if (hour >= 0 && hour <= 10 && minute <= 59 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                } else if (offSet < 0) {
+                    until2020DayAdjusted = until2020Day - 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 720) { //UTC: 12 hrs plus/minus.
+            if (hour >= 0 && hour <= 11 && minute <= 59 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                } else if (offSet < 0) {
+                    until2020DayAdjusted = until2020Day - 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 765) { //UTC: 12.75 hrs plus. No minus.
+            if (hour >= 0 && hour <= 12 && minute <= 44 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 780) { //UTC: 13 hrs plus. No minus.
+            if (hour >= 0 && hour <= 12 && minute <= 59 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                } else if (offSet < 0) {
+                    until2020DayAdjusted = until2020Day - 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        } else if (Math.abs(offSet) === 840) { //UTC: 14 hrs plus. No minus.
+            if (hour >= 0 && hour <= 13 && minute <= 59 && second <= 59) {
+                if (offSet > 0) {
+                    until2020DayAdjusted = until2020Day + 1;
+                }
+            } else {
+                until2020DayAdjusted = until2020Day;
+            }
+        }
+
     }
 
 }; //close variable
